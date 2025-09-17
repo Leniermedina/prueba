8e9a0f4034dcbe8887115594ebaa845b1a27c8ba
Lenier medina <leniermedina2003@gmail.com>
2025-09-17 12:45:01 -0400

    Delete Bum directory

--- Bum/src/app/(public)/page.tsx
+++
@@ -1,4 +0,0 @@
-import Hero from '@/components/home/Hero';
-import Featured from '@/components/home/Featured';
-import WhatsAppFab from '@/components/misc/WhatsAppFab';
-export default function HomePage(){return(<><Hero/><Featured/><WhatsAppFab/></>)}
